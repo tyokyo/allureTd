bb1cfc8882dffbdf0f7b2b8f739b5ae8663beb29
qiang.zhang <tyokyo@126.com>
2019-07-20 00:45:27 +0800

    allure like

---
+++ src/test/java/Demo.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Description;
+import ru.yandex.qatools.allure.annotations.TestCaseId;
+import ru.yandex.qatools.allure.annotations.Title;
+
+public class Demo {
+    @Test(invocationCount = 20)
+    @Title("aaa")
+    @Description("sssssssssssssssssssss")
+    @TestCaseId("111111111111")
+    public  void test(){
+
+    }
+}
